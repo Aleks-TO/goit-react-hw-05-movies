0698428426605fd01fc8e30894c5e3308c100934
Aleks-TO <114433644+Aleks-TO@users.noreply.github.com>
2023-07-06 22:06:35 +0300

    header complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
+
 function Header() {
-  return <div>Header</div>;
+  const ActiveClassName = ({ isActive }) =>
+    isActive ? css['active'] : css[navLink];
+  return (
+    <nav>
+      <div className={css.containerNav}>
+        <div className="navbar">
+          <NavLink className={ActiveClassName} to="/">
+            Home
+          </NavLink>
+          <NavLink className={ActiveClassName} to="/movies">
+            Movies
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,28 @@
-function MovieList() {
-  return <div>MovieList</div>;
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function MovieList({ movies }) {
+  const location = useLocation();
+
+  return (
+    <ul>
+      {movies.map(movie => (
+        <li
+          key={movie.id}
+          to={`/movies/:${movie.id}`}
+          state={{ from: Location }}
+        >
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+            {movie.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
